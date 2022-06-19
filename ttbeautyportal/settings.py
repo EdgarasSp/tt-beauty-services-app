d0c6de4849f1da7850eb829e9e0e977a5933152a
Edgaras Spiridonov <dev.applications.esp@gmail.com>
2022-06-19 00:25:38 +0000

    Add comment post Function

--- ttbeautyportal/settings.py
+++ ttbeautyportal/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'about',
     'services',
     'bookings',
@@ -67,6 +68,8 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
