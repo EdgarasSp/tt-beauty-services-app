2babeb6007a1669aabd501f6948cad04287c6a3d
Edgaras Spiridonov <dev.applications.esp@gmail.com>
2022-06-27 09:58:34 +0000

    Add about admin.py register and list filter

--- about/admin.py
+++ about/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import ContactForm
 
 # Register your models here.
 
-admin.site.register(ContactForm)
-    
+@admin.register(ContactForm)
+class ContactAdmin(SummernoteModelAdmin):
+    list_display = ('subject_type', 'first_name', 'last_name', 'email_address', 'received_date', 'message_problems')
+    list_filter = ('received_date',)
+    search_fields = ['subject_type', 'email_address', 'last_name']
+
+# admin.site.register(ContactForm)
--- about/urls.py
+++ about/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('about/', views.about_page, name='about'),
 
     path('manage', ManageTemplateView.as_view(), name='manage'),
-    path('contact/contact_form', SendContactFormTemplateView.as_view(), name='book_xray'),
+    path('contact/contact_form', SendContactFormTemplateView.as_view(), name='contact_form'),
 ]
