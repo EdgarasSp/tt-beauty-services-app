93e6a10ff4022335148f2d4181b20de2f740c365
Edgaras Spiridonov <dev.applications.esp@gmail.com>
2022-06-27 12:31:19 +0000

    Add contact admin status update function

--- about/admin.py
+++ about/admin.py
@@ -9,5 +9,9 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = ('subject_type', 'first_name', 'last_name', 'email_address', 'received_date', 'message_problems', 'status')
     list_filter = ('status', 'received_date')
     search_fields = ['subject_type', 'email_address', 'last_name']
+    actions = ['mark_completed']
+
+    def mark_completed(self, request, queryset):
+        queryset.update(status=True)
 
 # admin.site.register(ContactForm)
