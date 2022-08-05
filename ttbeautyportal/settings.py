1797b71e40947807490c4a11b030e2b1a76a622b
Edgaras Spiridonov <dev.applications.esp@gmail.com>
2022-08-05 12:00:03 +0000

    debug false

--- ttbeautyportal/settings.py
+++ ttbeautyportal/settings.py
@@ -17,7 +17,7 @@ if os.path.isfile('env.py'):
 '''
 THIS
 '''
-development = os.environ.get('DEVELOPMENT', True)
+development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
